f97a8ee76216082c50d1b39e889dacf432824bbf
Ferrel John Fernando <f.syntaxer@gmail.com>
2025-07-20 18:30:56 +0700

    chore: add app and cron folder

---
+++ cmd/app/app.go
@@ -0,0 +1,45 @@
+package app
+
+import (
+	"github.com/evalabs-id/go-clean-architecture/pkg/log"
+	"github.com/rs/zerolog"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+)
+
+type App struct {
+	Modules      []fx.Option
+	Servers      []any
+	Dependencies []any
+	Invokers     []any
+}
+
+func (app *App) AddModules(modules ...fx.Option) {
+	app.Modules = append(app.Modules, modules...)
+}
+
+func (app *App) AddServers(servers ...any) {
+	app.Servers = append(app.Servers, servers...)
+}
+
+func (app *App) AddDependencies(dependencies ...any) {
+	app.Dependencies = append(app.Dependencies, dependencies...)
+}
+
+func (app *App) AddInvokers(invokers ...any) {
+	app.Invokers = append(app.Invokers, invokers...)
+}
+
+func (app *App) Run() {
+	var opts = []fx.Option{}
+
+	opts = append(opts, app.Modules...)
+	opts = append(opts, fx.Provide(app.Dependencies...))
+	opts = append(opts, fx.Invoke(app.Invokers...))
+	opts = append(opts, fx.Invoke(app.Servers...))
+	opts = append(opts, fx.WithLogger(func(l zerolog.Logger) fxevent.Logger {
+		return log.Default(l)
+	}))
+
+	fx.New(opts...).Run()
+}
